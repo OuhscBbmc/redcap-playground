40d91e7476c6904a58e1f236dea25300051b308b
Will Beasley <wibeasley@hotmail.com>
2014-08-27 09:37:01 -0500

    using httr to call plugin

---
+++ call_delete_plugin_from_r.R
@@ -0,0 +1,11 @@
+require(httr)
+pathDeleteTestRecord <- "http://localhost/redcap/plugins/plugin_playground/noauth_delete.php"
+# httr::url_ok(pathDeleteTestRecord)
+
+#Returns a boolean value if successful
+(was_successful <- httr::url_success(pathDeleteTestRecord))
+
+#Returns much more information (that needs to parsed
+(r <- httr::GET(pathDeleteTestRecord))
+str(r)
+content(r)
